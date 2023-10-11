e421f2a43be966fa78c8077e6de4b4e581466c0c
Jayasanka <jayasanka.sack@gmail.com>
2023-10-11 14:26:18 -0700

    Use nonroot playwright container

--- e2e/support/bamboo/e2e-test-runner.sh
+++ e2e/support/bamboo/e2e-test-runner.sh
@@ -3,7 +3,7 @@
 export E2E_BASE_URL=http://gateway/openmrs
 export CI=true
 
-timeout=$((SECONDS + 900))  # Timeout after 15 minutes (900 seconds)
+#timeout=  # Timeout after 15 minutes (900 seconds)
 
 yarn install --immutable
 
@@ -11,10 +11,10 @@ while [ "$(curl -s -o /dev/null -w ''%{http_code}'' http://gateway/openmrs/login
   echo "Waiting for the backend to be up..."
   sleep 10
 
-  if [ $SECONDS -gt $timeout ]; then
-    echo "Timeout reached. The backend did not become available within 15 minutes."
-    exit 1
-  fi
+#  if [ $SECONDS -gt $timeout ]; then
+#    echo "Timeout reached. The backend did not become available within 15 minutes."
+#    exit 1
+#  fi
 done
 
 yarn test-e2e
---
+++ e2e/support/bamboo/playwright.Dockerfile
@@ -0,0 +1,3 @@
+# syntax=docker/dockerfile:1.3
+FROM mcr.microsoft.com/playwright:v1.34.0-jammy
+USER 1001
