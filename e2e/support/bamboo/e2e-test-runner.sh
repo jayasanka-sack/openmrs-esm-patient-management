6119140ddaa6b4f9afc3987dcf1d414b7c912109
Jayasanka <jayasanka.sack@gmail.com>
2023-10-11 16:44:49 -0700

    Use dev3

--- e2e/support/bamboo/e2e-test-runner.sh
+++ e2e/support/bamboo/e2e-test-runner.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
-export E2E_BASE_URL=http://gateway/openmrs
+export E2E_BASE_URL=https://dev3.openmrs.org/openmrs
 export CI=true
 
-while [ "$(curl -s -o /dev/null -w ''%{http_code}'' $E2E_BASE_URL/login.htm)" != "200" ]; do
-  echo "Waiting for the backend to be up..."
-  sleep 10
-
-done
+#while [ "$(curl -s -o /dev/null -w ''%{http_code}'' http://gateway/openmrs/login.htm)" != "200" ]; do
+#  echo "Waiting for the backend to be up..."
+#  sleep 10
+#
+#done
 
 npx playwright test -g "View active visits"
