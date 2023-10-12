7c72c1684dfa475cf9c7a4dfad39b76a3bb4bf69
Jayasanka <jayasanka.sack@gmail.com>
2023-10-11 20:02:53 -0700

    ss

--- e2e/support/bamboo/e2e-test-runner.sh
+++ e2e/support/bamboo/e2e-test-runner.sh
@@ -5,7 +5,6 @@ export CI=true
 
 while [ "$(curl -s -o /dev/null -w ''%{http_code}'' http://gateway/openmrs/login.htm)" != "200" ]; do
   echo "Waiting for the backend to be up..."
-  docker-compose logs --tail 20
   sleep 10
 done
 
